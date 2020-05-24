fe9fa971772832725228c95225d61f062353be72
Blaine Price <blaine@ario.com>
2020-05-24 12:38:25 -0400

    Cleanup

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,9 +1,35 @@
-mod household;
-mod person;
-mod rsvp;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-pub use self::{
-    household::{Household, HouseholdRecord},
-    person::{Contact, Person},
-    rsvp::RSVP,
-};
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Contact {
+    Email { value: String },
+    SMS { value: String },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Person {
+    pub household_id: Uuid,
+    pub name: String,
+    pub contact: Contact,
+    pub rsvp: Option<bool>,
+    pub dietary_restrictions: Option<DietaryRestrictions>,
+    pub dish_preference: Option<DishPreference>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DietaryRestrictions {
+    Vegetarian,
+    Vegan,
+    Pescetarian,
+    GlutenFree,
+    DairyFree,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DishPreference {
+    Chicken,
+    Steak,
+    Pancakes,
+    Pizza    
+}
--- services/src/household.rs
+++ services/src/household.rs
@@ -1,5 +1,5 @@
-use anyhow::Result;
-use models::{Household, HouseholdRecord, Person};
+use anyhow::{bail, Result};
+use models::Person;
 use rusoto_core::Region;
 use rusoto_dynamodb::{
     AttributeValue, BatchWriteItemInput, DynamoDb, DynamoDbClient, PutRequest, QueryInput,
@@ -24,24 +24,16 @@ impl HouseholdService {
         }
     }
 
-    pub async fn put(self, people: Vec<Person>) -> Result<Household> {
-        let household = Household {
-            id: Uuid::new_v4(),
-            people: people.clone(),
-        };
+    pub async fn create(self, people: Vec<Person>) -> Result<Vec<Person>> {
+        if people.is_empty() {
+            bail!("A household must have at least one person")
+        }
 
-        let put_requests: Vec<WriteRequest> = household
-            .people
+        let put_requests: Vec<WriteRequest> = people
             .iter()
             .map(|person| WriteRequest {
                 put_request: Some(PutRequest {
-                    item: serde_dynamodb::to_hashmap(&HouseholdRecord {
-                        household_id: household.id,
-                        contact: person.contact.clone(),
-                        name: person.name.clone(),
-                        rsvp: person.rsvp.clone(),
-                    })
-                    .unwrap(),
+                    item: serde_dynamodb::to_hashmap(person).unwrap(),
                 }),
                 ..WriteRequest::default()
             })
@@ -58,11 +50,13 @@ impl HouseholdService {
         self.client
             .batch_write_item(batch_write_request_input)
             .await?;
-        Ok(household)
+
+        Ok(people)
     }
 
-    pub async fn get(self, household_id: Uuid) -> Result<Option<Household>> {
+    pub async fn read(self, household_id: Uuid) -> Result<Option<Vec<Person>>> {
         let mut query = HashMap::new();
+
         query.insert(
             String::from(":household_id"),
             AttributeValue {
@@ -80,27 +74,16 @@ impl HouseholdService {
 
         let response = self.client.query(query_input).await?;
         if let Some(items) = response.items {
-            if !items.is_empty() {
-                let household_records: Vec<HouseholdRecord> = items
-                    .into_iter()
-                    .map(|item| serde_dynamodb::from_hashmap(item).unwrap())
-                    .collect();
-                let household_id: Uuid = household_records[0].household_id;
-
-                let people: Vec<Person> = household_records
-                    .into_iter()
-                    .map(|record| Person {
-                        name: record.name,
-                        contact: record.contact,
-                        rsvp: record.rsvp,
-                    })
-                    .collect();
-
-                return Ok(Some(Household {
-                    id: household_id,
-                    people
-                }));
+            if items.is_empty() {
+                return Ok(None);
             }
+
+            let people: Vec<Person> = items
+                .into_iter()
+                .map(|item| serde_dynamodb::from_hashmap(item).unwrap())
+                .collect();
+
+            return Ok(Some(people));
         }
         Ok(None)
     }
@@ -121,56 +104,70 @@ mod tests {
     #[tokio::test]
     async fn it_should_create_a_household() {
         let service = HouseholdService::new();
+        let household_id = Uuid::new_v4();
         let people = vec![
             Person {
+                household_id,
                 name: "John".to_string(),
                 contact: Contact::Email {
                     value: "hello@example.com".to_string(),
                 },
                 rsvp: None,
+                dietary_restrictions: None,
+                dish_preference: None,
             },
             Person {
+                household_id,
                 name: "Sally".to_string(),
                 contact: Contact::SMS {
                     value: "5555555555".to_string(),
                 },
                 rsvp: None,
+                dietary_restrictions: None,
+                dish_preference: None,
             },
         ];
 
-        let household = service.put(people).await.unwrap();
-        assert_eq!(household.people.len(), 2 as usize);
+        let guests = service.create(people).await.unwrap();
+        assert_eq!(guests.len(), 2 as usize);
     }
 
     #[tokio::test]
     async fn it_should_get_a_household() {
+        let household_id = Uuid::new_v4();
         let people = vec![
             Person {
+                household_id,
                 name: "John".to_string(),
                 contact: Contact::Email {
                     value: "hello@example.com".to_string(),
                 },
                 rsvp: None,
+                dietary_restrictions: None,
+                dish_preference: None,
             },
             Person {
+                household_id,
                 name: "Sally".to_string(),
                 contact: Contact::SMS {
                     value: "5555555555".to_string(),
                 },
                 rsvp: None,
+                dietary_restrictions: None,
+                dish_preference: None,
             },
         ];
-        let put_household = HouseholdService::new().put(people).await.unwrap();
+        HouseholdService::new().create(people).await.unwrap();
 
-        match HouseholdService::new().get(put_household.id).await.unwrap() {
-            Some(household) => assert_eq!(household.people.len(), 2 as usize),
+        match HouseholdService::new().read(household_id).await.unwrap() {
+            Some(guests) => assert_eq!(guests.len(), 2 as usize),
             None => assert!(false),
         }
     }
 
     #[tokio::test]
     async fn it_should_not_get_a_household() {
-        match HouseholdService::new().get(Uuid::new_v4()).await.unwrap() {
+        match HouseholdService::new().read(Uuid::new_v4()).await.unwrap() {
             Some(_household) => assert!(false),
             None => assert!(true),
         }
